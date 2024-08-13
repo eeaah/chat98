cad5b6b3d41e4bfadc9c9ac8d03123f5a77ecae1
Ian Prigmore <ianaprigmore@gmail.com>
2024-08-12 23:44:56 -0500

    Implemented kaomoji editor

--- src/components/MessageBox/KaomojiOption.jsx
+++ src/components/MessageBox/KaomojiOption.jsx
@@ -1,8 +1,24 @@
 import { useState } from "react";
-import styles from "./MessageBox.module.css"
+import styles from "./MessageBox.module.css";
 
-function KaomojiOption({ text, index }) {
-	return <button className={styles.Kaomoji_option}>{text}</button>;
+function KaomojiOption({ text, index, tab, sendKaomoji }) {
+	let display = "";
+	if(tab == 3) {
+		display = text[0] + "  " + text[1];
+	} else if(tab == 4) {
+		display = text[0] + "     " + text[1];
+	} else if(tab == 0 || tab == 2){
+		display = text;
+	}
+
+	return (
+		<button
+			onClick={() => sendKaomoji(text)}
+			className={styles.Kaomoji_option}
+		>
+			{display}
+		</button>
+	);
 }
 
 export default KaomojiOption;
--- src/components/MessageBox/MessageBox.jsx
+++ src/components/MessageBox/MessageBox.jsx
@@ -8,6 +8,14 @@ import styles from "./MessageBox.module.css";
 function MessageBox({ userDetails }) {
 	const [message, setMessage] = useState("");
 	const [kaomojiTab, setKaomojiTab] = useState(0);
+	const [currentKaomoji, setCurrentKaomoji] = useState([
+		"(´",
+		"•",
+		"ω",
+		"•",
+		"`)",
+	]);
+	const [recentKaomoji, setRecentKaomoji] = useState([]);
 
 	const handleKaomojiTab = (event) => {
 		setKaomojiTab(event.target.id);
@@ -31,6 +39,41 @@ function MessageBox({ userDetails }) {
 		}
 	};
 
+	const sendCurKaomoji = () => {
+		setMessage(message + currentKaomoji.join(""));
+		recent = recentKaomoji;
+		if (recent.length > 16) {
+			recent = recend.slice(0, recent.length - 1);
+		}
+		recent = [currentKaomoji, ...recent];
+		setRecentKaomoji(recent);
+	};
+
+	const sendKaomoji = (text) => {
+		if (kaomojiTab == 0) {
+			setMessage(message + text);
+		} else if (kaomojiTab == 2) {
+			const newFace = [
+				...currentKaomoji.slice(0, 2),
+				text,
+				...currentKaomoji.slice(3),
+			];
+			setCurrentKaomoji(newFace);
+		} else if (kaomojiTab == 3) {
+			const newFace = [
+				...currentKaomoji.slice(0, 1),
+				text[0],
+				...currentKaomoji.slice(2, 3),
+				text[1],
+				...currentKaomoji.slice(4),
+			];
+			setCurrentKaomoji(newFace);
+		} else if (kaomojiTab == 4) {
+			const newFace = [text[0], ...currentKaomoji.slice(1, 4), text[1]];
+			setCurrentKaomoji(newFace);
+		}
+	};
+
 	return (
 		<div className={styles.form}>
 			<label htmlFor="messageInput" hidden>
@@ -40,6 +83,7 @@ function MessageBox({ userDetails }) {
 				id="messageInput"
 				name="messageInput"
 				type="text"
+				maxlength="256"
 				value={message}
 				onChange={(e) => setMessage(e.target.value)}
 				className={styles.form__input}
@@ -49,7 +93,9 @@ function MessageBox({ userDetails }) {
 					Send
 				</button>
 				<button className={styles.btn}>Preview</button>
-				<button className={styles.btn}>◕ ◡ ◕</button>
+				<button onClick={sendCurKaomoji} className={styles.btn}>
+					{currentKaomoji}
+				</button>
 			</div>
 			<div className={`${styles.flex} ${styles.kaomoji_container}`}>
 				<button
@@ -68,7 +114,7 @@ function MessageBox({ userDetails }) {
 						kaomojiTab == 1 ? styles.kaomoji_tab_active : ""
 					}`}
 				>
-					Mouth
+					Recent
 				</button>
 				<button
 					id={2}
@@ -77,7 +123,7 @@ function MessageBox({ userDetails }) {
 						kaomojiTab == 2 ? styles.kaomoji_tab_active : ""
 					}`}
 				>
-					Eyes
+					Mouth
 				</button>
 				<button
 					id={3}
@@ -86,7 +132,7 @@ function MessageBox({ userDetails }) {
 						kaomojiTab == 3 ? styles.kaomoji_tab_active : ""
 					}`}
 				>
-					Body
+					Eyes
 				</button>
 				<button
 					id={4}
@@ -95,11 +141,18 @@ function MessageBox({ userDetails }) {
 						kaomojiTab == 4 ? styles.kaomoji_tab_active : ""
 					}`}
 				>
-					Arms
+					Body
 				</button>
 			</div>
 			<div className={styles.kaomoji_field}>
-				{kaomojiList[0]?.map((text, index) => (<KaomojiOption text={text} index={index}/>))}
+				{kaomojiList[kaomojiTab]?.map((text, index) => (
+					<KaomojiOption
+						text={text}
+						index={index}
+						tab={kaomojiTab}
+						sendKaomoji={sendKaomoji}
+					/>
+				))}
 			</div>
 		</div>
 	);
