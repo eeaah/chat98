dece8cc8fbcce09bbfef1df79a429f360e3e68ae
Ian Prigmore <ianaprigmore@gmail.com>
2024-09-16 01:08:52 -0500

    Github pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://eeaah.github.io/chat98/",
+  base: "/chat98/",
 });
