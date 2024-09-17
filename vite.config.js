a8bef92c052ff4adb20dd2225f356cea781cb9e3
Ian Prigmore <ianaprigmore@gmail.com>
2024-09-16 23:59:44 -0500

    messing with stuff again :(

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/chat98/",
+	plugins: [react()],
+	base: "https://eeaah.github.io/chat98/",
 });
